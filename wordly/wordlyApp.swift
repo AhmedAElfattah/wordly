f5cca82fbac9ab68acc2e4845fdc82e5125f67a0
AhmedMAbdelfattah <ahmed.abdelfattah@yassir.com>
2025-04-14 19:04:47 +0200

    Initial Commit

---
+++ wordly/wordlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordlyApp.swift
+//  wordly
+//
+//  Created by Ahmed on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct wordlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
